29d35728bf9eefd8eabe04215bf8d5b0aaa8736e
Your Name <gourabbanik2399@gmail.com>
2021-11-01 03:32:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,309 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App">
+      <div class="d-flex justify-content-between container">
+        <div>
+          <p class="title"> <span class="title-span">T</span>he </p>
+
+          <p class="title"><span class="title-span">E</span>ntrepreneurship </p>
+
+          <p class="title1"><span class="title-span">N</span>etwork </p>
+
+
+        </div>
+        <div >
+
+          <div class="d-flex m-3"><i class="fas fa-shopping-basket"></i>
+            <div class="d-flex box">
+              <i class="fas fa-user"></i>
+              <p>Log In</p>
+            </div>
+          </div>
+          <form class="form-inline">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+
+      <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light ">
+          <a class="navbar-brand" href="#"><img src="https://static.wixstatic.com/media/693073_ee31d3dcd4064037a097ec905d0c841b~mv2.jpg/v1/fill/w_125,h_100,al_c,q_80,usm_0.66_1.00_0.01/pp_edited.webp" /></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-item nav-link active text-danger" href="#">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-item nav-link active" href="#">Features</a>
+              <a class="nav-item nav-link active" href="#">Pricing</a>
+              <a class="nav-item nav-link active" href="#">Disabled</a>
+            </div>
+          </div>
+        </nav>
+
+      </div>
+
+
+
+
+
+
+      <div class="background-img">
+
+        <h1 class="background-h1">A World of Knowledge at Your Fingertips</h1>
+      </div>
+
+      <div class="teaser">
+        <h3 class="teaser-title">About Us</h3>
+        <div class="teaser-2-side">
+          <div>
+
+            <h3 class="teaser-h3">Learning Together
+              From the Comfort
+              of Your Home</h3>
+            <button class="teaser-button">
+              Learn More
+            </button>
+          </div>
+          <div>
+            <img class="teaser-img"
+              src="https://images.unsplash.com/photo-1502444330042-d1a1ddf9bb5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z3JvdXAlMjBzdmd8ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
+          </div>
+        </div>
+      </div>
+
+
+      <div>
+        <div>
+          <div class="parallax">
+            <h5 class="welcome">Welcome to</h5>
+            <h1 class="h11">THE NEXT BIG THING</h1>
+            <button class="parallax-btn">Join Ten Vertual Campus</button>
+          </div>
+        </div>
+        <div class="parallax"></div>
+      </div>
+
+
+      <div class="teaser1">
+        <div class="containerteaser">
+          <img class="containerteaserimg"
+            src="https://images.unsplash.com/photo-1502444330042-d1a1ddf9bb5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z3JvdXAlMjBzdmd8ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
+          <div class="centered1">
+            <a class="teaser1button">Our Course</a>
+          </div>
+          <div class="centered">
+            <p>Your New</p>
+            <p>Journey Begins Here,</p>
+            <p>Today</p>
+          </div>
+        </div>
+        <div class="containerteaser1">
+
+          <h3 class="centered2">Entrepreneurship
+            made fun</h3>
+          <button class="centered2btn">Learn More</button>
+
+        </div>
+      </div>
+
+      <div class="teaser2">
+        <div class="teaser2div1"> <img class="containerteaserimg1"
+          src="https://static.wixstatic.com/media/44a6c2_f9b99e49448943108aa1a3273b1c356b~mv2.jpg/v1/fill/w_850,h_563,al_c,q_85,usm_0.66_1.00_0.01/44a6c2_f9b99e49448943108aa1a3273b1c356b~mv2.webp" /></div>
+        <div class="teaser2div2">
+
+          <h3 class="centered2">Entrepreneurship
+            made fun</h3>
+          <button class="centered2btn">Learn More</button>
+        </div>
+        <div class="teaser2div3">
+          <img class="containerteaserimg1"
+            src="https://static.wixstatic.com/media/11062b_557462ab123440368447ab18a66043e5~mv2.jpg/v1/fill/w_425,h_563,al_c,q_80,usm_0.66_1.00_0.01/11062b_557462ab123440368447ab18a66043e5~mv2.webp" />
+        </div>
+      </div>
+
+
+      <div class="teaser3">
+        <div class="teaser3div2">
+
+          <h3 class="centered2"><a class="teaser3div2a">Product Management
+            like a pro</a> </h3>
+          <button class="centered3btn">Learn More</button>
+        </div>
+        <div class="teaser3div3">
+          <img class="containerteaserimg1"
+            src="https://static.wixstatic.com/media/a3ac449c86364dd8999922949876d09e.jpg/v1/fill/w_850,h_555,al_c,q_85,usm_0.66_1.00_0.01/a3ac449c86364dd8999922949876d09e.webp" />
+        </div>
+      </div>
+
+
+      <div class="h1div">
+        <h1>Our Course Benefits</h1>
+      </div>
+
+
+
+      <div class="container justify-content-around">
+        <div class="row">
+          <div class="col-lg">
+            <i class="fas fa-minus-square"></i>
+            <h2>Expert Teachers</h2>
+            <p>Our instructors are more adept at monitoring student
+              problems and assessing their level of understanding and
+              progress, and they provide much more relevant, useful
+              feedback.
+              We are experts in developing and testing hypotheses
+              about learning difficulties or instructional strategies.</p>
+          </div>
+          <div class="col-lg">
+            <i class="fab fa-dribbble-square"></i>
+            <h2>Online Community</h2>
+            <p>Feel like home, with a "family of invisible friends".</p>
+          </div>
+          <div class="col-lg">
+            <i class="fas fa-th"></i>
+            <h2>Flexible Curriculum</h2>
+            <p>Our expert teachers design and curate the curriculum
+              using best practices and careful consideration of how
+              people learn and retain information with the ongoing
+              tech trends in the professional domain. We've debated
+              every facet of our methodology, from the order in which
+              to teach concepts, the analogies used to clarify them
+              and how exercises should be structured to deliver the
+              maximum educational punch.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="h2div">
+        <h1>Te<span>stimonia</span>ls</h1>
+      </div>
+
+
+
+      <div id="carouselExampleControls" class="carousel slide"
+        data-ride="carousel">
+        <div class="carousel-inner text-justify text-center">
+          <div class="carousel-item active">
+            <div>
+              <img
+                src="https://static.wixstatic.com/media/11062b_558f00357585424e894fb7c595fc38fd~mv2.jpg/v1/fill/w_1520,h_620,al_c,q_85,usm_0.66_1.00_0.01/11062b_558f00357585424e894fb7c595fc38fd~mv2.webp" />
+              <h1 class="carousel-caption mb-lg-5 p-lg-5">Working with
+                TEN as a digital marketing intern has been a great
+                experience. I learnt a lot of new stuff and worked
+                on different tasks which helped me understand the
+                marketing field better.</h1>
+              <h2 class="carousel-caption p-lg-5"> -Harsh Rajput,
+                General Management Intern</h2>
+            </div>
+
+
+          </div>
+          <div class="carousel-item">
+            <img
+              src="https://static.wixstatic.com/media/11062b_558f00357585424e894fb7c595fc38fd~mv2.jpg/v1/fill/w_1520,h_620,al_c,q_85,usm_0.66_1.00_0.01/11062b_558f00357585424e894fb7c595fc38fd~mv2.webp"
+            />
+            <h2 class="carousel-caption mb-lg-5 p-lg-5">I thoroughly
+              enjoyed my internship during this pandemic situation and
+              gained valuable experience under my belt. I know this
+              internship will be instrumental in getting myself a job
+              and it would be glowing reference on my CV. I was very
+              hesitant at the time of joining TEN due to my
+              inexperience, but now I feel so happy that I was able to
+              be a part of it as it has made me so much more confident
+              and empowered. As much as this organization changes, I
+              hope that it’s class remains constant.
+
+              Thank you team TEN!</h2>
+            <h2 class="carousel-caption">-Kshema Unni, Business
+              Development Intern</h2>
+          </div>
+          <div class="carousel-item">
+            <img
+              src="https://static.wixstatic.com/media/11062b_558f00357585424e894fb7c595fc38fd~mv2.jpg/v1/fill/w_1520,h_620,al_c,q_85,usm_0.66_1.00_0.01/11062b_558f00357585424e894fb7c595fc38fd~mv2.webp"
+            />
+            <h2 class="carousel-caption mb-lg-5 p-lg-5">Working with TEN
+              is a great opportunity for the students who want to
+              extend their career in digital marketing!</h2>
+            <h2 class="carousel-caption">-Aakriti Malik, DM Intern</h2>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls"
+          role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls"
+          role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+
+
+      <div class="videosection" >
+
+        <video autoPlay muted id="myVideo">
+          <source src="./Videos/video1.mp4" type="video/mp4" />
+          Your browser does not support HTML5 video.
+        </video>
+
+        <div class="content">
+          <h1>Power of TEN Consulting</h1>
+          <button id="myBtn" ><span class="btnspan">Explore Here </span> <i class="fas fa-chevron-right"></i></button>
+        </div>
+
+
+      </div>
+
+
+      <div class="d-flex justify-content-around">
+        <div class="text-center">
+          <i class="fab fa-facebook-square"></i>
+          <i class="fab fa-twitter"></i>
+          <i class="fab fa-instagram"></i>
+        </div>
+        <div class="text-center">
+          <p>Subscribe to our newsletter</p>
+          <div>
+            <input class="inputdiv" type="text" placeholder="Full Name" />
+            <input class="inputdiv" type="text" placeholder="College Name" />
+          </div>
+          <div>
+            <input class="inputdiv" type="text" placeholder="Mobile Number" />
+            <input class="inputdiv" type="email" placeholder="Email" />
+          </div>
+
+          <input type="checkbox" id="vehicle2" name="vehicle2" value="Car" />
+          <label for="vehicle2">Join TEN Discord community</label>
+          <input class="inputsubmit" type="submit" value="Submit" />
+
+        </div>
+        <div class="abouttext">
+          <div>
+            <a>About Us</a>
+          </div>
+          <div>
+            <a>FAQ</a>
+          </div>
+          <div>
+            <a>Contact Us</a>
+          </div>
+
+
+
+        </div>
+      </div>
+
+
+
+    </div>
+  );
+}
+
+export default App;
